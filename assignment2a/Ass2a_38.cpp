009199d0a62cc4a2bdd90619bd78e6e5cd894813
theerdha <sreetheerdha9@gmail.com>
2018-01-27 17:45:58 +0530

    Adds full impementation of producer and consumer processes

--- assignment2a/Ass2a_38.cpp
+++ assignment2a/Ass2a_38.cpp
@@ -14,39 +14,27 @@
 
 using namespace std;
 
-int prime_numbers[] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,101, 103, 107, 109, 113, 127, 131, 137, 139, 149,151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199};
+int prime_numbers[] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 
+	73, 79, 83, 89, 97,101, 103, 107, 109, 113, 127, 131, 137, 139, 149,151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199};
 
-
-
-int main()
+void producers(int NP, vector<int> pids,int* parentseg)
 {
-	clock_t begin = clock();	
-	int NP,NC,status;
-	cout << "enter number of producers : "; cin >> NP;
-	cout << "enter number of customers : "; cin >> NC;
-
-	int r,w,success,element,value;
-
-	int shmid = shmget(key, 5 * sizeof(int), IPC_CREAT|0700);
-	int* parentseg = (int *) shmat(shmid,NULL,0);
-	parentseg[0] = -1;parentseg[1] = -1;parentseg[2] = -1;parentseg[3] = -1;parentseg[4] = -1;
-
-	vector<int> pids;
-
-	for(int i = 1; i <= NP + NC; i++){
-		if(fork() == 0){
-			srand (time(NULL) + i);
-			//int* myseg = (int *)shmat(shmid,NULL,0);
-			w = rand() % 5;
-
-			if(i <= NP){
+	int X = fork();
+	if(X == 0)
+	{
+		for(int i = 1; i <= NP; i++)
+		{
+			if(fork() == 0)
+			{
+				srand (time(NULL) + i);
+				int w = rand() % 5;
 				pids.push_back((int)getpid());
-				r = rand() % 45;
+				int r = rand() % 45;
 				r = prime_numbers[r];
 				
-				 success = 0; element = 0;	
-				 usleep(w * 1000);
-				 while(!success){
+				int success = 0;	
+				sleep(w);
+				while(!success){
 					for(int j = 0; j < 5; j++){
 						if(parentseg[j] == -1) {
 							parentseg[j] = r;
@@ -55,14 +43,35 @@ int main()
 						}
 					}
 				}
-				cout << "Producer" << i  << ": " << r << " ,time : " << endl; 			
-				
+				const time_t ctt = time(0);
+				cout << "Producer" << i  << ": " << r << " ,time : " << asctime(localtime(&ctt)) << endl; 	
 			}
 
-			else{
+			else
+			{
+				wait(NULL);
+			}
+		}
+	}
+	else return;
+}
+
+
+void consumers(int NC, vector<int> pids,int* parentseg)
+{
+	int X = fork();
+	if(X == 0)
+	{
+		for(int i = 1; i <= NC; i++)
+		{
+			if(fork() == 0)
+			{
+				srand (time(NULL) + i + 10);
+				int w = rand() % 5;
 				pids.push_back((int)getpid());
-				usleep(w * 1000);
-				success = 0;
+				sleep(w);
+				int success = 0;
+				int value;
 				while(!success){
 					for(int j = 0; j < 5; j++){
 						if(parentseg[j] != -1) {
@@ -74,33 +83,34 @@ int main()
 						
 					}
 				}
-				cout << "Consumer" << i - NP << ": " << value << " ,time : " << endl;
+				const time_t ctt = time(0);
+				cout << "Consumer" << i << ": " << value << " ,time : " << asctime(localtime(&ctt)) << endl;
 			}
-			
 
-		}
-
-		else{
-			clock_t end = clock();
-			//parent(pids,begin);
-			double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
-
-			if(elapsed_secs >= 30){
-				for(int k = 0; k < pids.size(); k++){
-					kill(pids[k],SIGQUIT);
-				}
-				shmdt(parentseg);
-				shmctl(shmid, IPC_RMID, 0);
-				_exit(0);
+			else
+			{
+				wait(NULL);
 			}
-			//wait(&status);
-			break;
-			
 		}
+	}
 
-	}	
-
-	 for(int i = 0; i < NP + NC; i++) // loop will run n times (n=5)
-     wait(NULL);
-
+	else return;
 }
+
+int main()
+{	
+	int NP,NC,status;
+	cout << "enter number of producers : "; cin >> NP;
+	cout << "enter number of customers : "; cin >> NC;
+	int shmid = shmget(key, 5 * sizeof(int), IPC_CREAT|0700);
+	int* parentseg = (int *) shmat(shmid,NULL,0);
+	parentseg[0] = -1;parentseg[1] = -1;parentseg[2] = -1;parentseg[3] = -1;parentseg[4] = -1;
+	vector<int> pids;
+
+	producers(NP,pids,parentseg);
+	consumers(NC,pids,parentseg);
+	sleep(20);
+	kill(0,SIGTERM);
+	return 0;
+}
+	
