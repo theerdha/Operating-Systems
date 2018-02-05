0c0c1967f35bb6dbcb936039c3645333b0c45aa0
Buridi Aditya <buridiaditya@iitkgp.ac.in>
2018-02-05 17:27:36 +0530

    Adds new plotting function

--- assignment3a/plot.py
+++ assignment3a/plot.py
@@ -26,31 +26,34 @@ y = input()
 y_axis5 = np.array(y.split(' ')).astype(float)
 
 
-f, (ax1, ax2, ax3,ax4,ax5) = plt.subplots(5, sharex=True, sharey=True)
+f, ax1 = plt.subplots()
 
-ax1.plot(x_axis, y_axis1, 'o-')
-ax1.set_title('ATN time vs N')
-ax1.set_ylabel('FCFS ATN time')
+ax1.plot(x_axis, y_axis1, 'r^-',label='FCFS')
+ax1.set_title('ATN time vs N for differnt schedulers')
+#ax1.set_ylabel('FCFS ATN time')
 
 ax1.grid()
-ax2.plot(x_axis, y_axis2, 'o-')
-ax2.set_ylabel('SJF ATN time')
+ax1.plot(x_axis, y_axis2, 'bo-',label='SJF')
+#ax2.set_ylabel('SJF ATN time')
 
-ax2.grid()
-ax3.plot(x_axis, y_axis3, 'o-')
-ax3.set_ylabel('RR1 ATN time')
+#ax2.grid()
+ax1.plot(x_axis, y_axis3, 'go-',label='RR1')
+#ax3.set_ylabel('RR1 ATN time')
 
-ax3.grid()
-ax4.plot(x_axis, y_axis4, 'o-')
-ax4.set_ylabel('RR2 ATN time')
+#ax3.grid()
+ax1.plot(x_axis, y_axis4, 'y^-',label='RR2')
+#ax4.set_ylabel('RR2 ATN time')
 
-ax4.grid()
-ax5.plot(x_axis, y_axis5, 'o-')
-ax5.set_ylabel('RR3 ATN time')
-ax5.set_xlabel('N values')
-ax5.grid()
+#ax4.grid()
+ax1.plot(x_axis, y_axis5, 'k^-',label='RR5')
+ax1.set_ylabel('ATN time')
+ax1.set_xlabel('N values')
+#ax5.grid()
 
-f.subplots_adjust(hspace=0.05)
-plt.setp([a.get_xticklabels() for a in f.axes[:-1]], visible=False)
+legend = ax1.legend(loc='upper left', shadow=True, fontsize='x-large')
+
+# Put a nicer background color on the legend.
+legend.get_frame().set_facecolor('#00FFCC')
+
 
 plt.show()
