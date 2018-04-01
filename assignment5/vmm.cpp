6a26ac598a944436dbd33fd5cd9cdb470a5ba822
theerdha <sreetheerdha9@gmail.com>
2018-04-01 18:19:03 +0530

    optimizes the code my changing page table structure

--- assignment5/vmm.cpp
+++ assignment5/vmm.cpp
@@ -18,7 +18,7 @@ typedef struct INSTR
 }instruction; 
 
 vector<instruction> IR_TABLE;
-char page_table[64][4];
+int page_table[64];
 vector<int> freeList; //--> 1 : free , 0 : occupied
 vector<int> frameLine; //--> -1 : free , else line number at which it is loaded
 queue <int> page_FIFO;
@@ -43,42 +43,34 @@ int RandomPR(int * phy)
 	while(1)
 	{
 		vir = rand() % 64;
-		x1 = (int *)(page_table[vir]);
-		b1 = *x1;
-		b2 = b1 % 8;
 		// if((page_table[vir][3] | 0xFB) == 0xFF) break;
-	    if(b2 >= 4) break;
+	    if((page_table[vir] % 8) >= 4) break;
 	}
-	*phy = b1/8;
+	*phy = page_table[vir]/8;
 	return vir;
 }
 
 int NRU(int * phy)
 {
-	int * x1; int b1,b2,b3,vir = -1;
+	int vir = -1;
 	if(PAGE_FAULTS % 10 == 0)
 	{
 		for(int i = 0; i < 64; i++) 
 		{
-			x1 = (int *)(page_table[i]);
-			b1 = *x1;
-			b1 -= 1;
-			*x1 = b1;
+			if((page_table[i]) % 2 == 1)page_table[i] -= 1;
+			
 		}
 	}
 
 	vector<int> category0;vector<int> category1;vector<int> category2;vector<int> category3;
 	for(int i = 0; i < 64; i++) 
 	{
-		x1 = (int *)(page_table[i]);
-		b2 = *x1;
-		b3 = b2 % 8;
-		b1 = b2 % 4;
-		if(b3 >= 4)
+		
+		if((page_table[i] % 8) >= 4)
 		{
-			if(b1 == 0)category0.push_back(i);
-			else if(b1 == 1)category2.push_back(i);
-			else if(b1 == 2)category1.push_back(i);
+			if((page_table[i] % 4) == 0)category0.push_back(i);
+			else if((page_table[i] % 4) == 1)category2.push_back(i);
+			else if((page_table[i] % 4) == 2)category1.push_back(i);
 			else category3.push_back(i);
 		}
 	}
@@ -88,26 +80,18 @@ int NRU(int * phy)
 	else if(category2.size() != 0) vir = category2[(rand() % (category2.size()))];
 	else vir = category3[(rand() % (category3.size()))];
 	
-	x1 = (int *)(page_table[vir]);
-	b1 = *x1;
-	b1 /= 8;
-	*phy = b1;
+	*phy = page_table[vir]/8;
 	return vir;
 }
 
 int LRU(int * phy)
 {
-	int vir,b1;
-	int * x1;
+	int vir;
 	vir = page_LRU.front();
 	page_LRU.pop_front();
-	x1 = (int *)(page_table[vir]);
-	b1 = *x1;
-	int b3 = b1/8;
-	int b2 = b1 % 8;
-	if(b2 >= 4)
+	if((page_table[vir] % 8) >= 4)
 	{
-		*phy = b3;
+		*phy = page_table[vir] /8;
 	}
 	return vir;
 }
@@ -115,52 +99,30 @@ int LRU(int * phy)
 int FIFO(int * phy)
 {
 	int vir,b1;
-	int *x1;
-	// int min = 10000; 
-	// int minindex;
-	// for(int i = 0; i < frameLine.size(); i++)
-	// {
-	// 	if(frameLine[i] < min)
-	// 	{
-	// 		min = frameLine[i];
-	// 		minindex = i;
-	// 	}
-	// }
-	// *phy = minindex;
 	vir = page_FIFO.front();
 	page_FIFO.pop();
-	
-	x1 = (int *)(page_table[vir]);
-	b1 = *x1;
-	int b3 = b1/8;
-	int b2 = b1 % 8;
-	*phy = b3;
-	
+	*phy = page_table[vir] / 8;
 	return vir;
 }
 
 int SecChance(int * phy)
 {
-	int x;int vir,b1,b2;
-	int *x1;
+	int x, vir;
 
 	while(1)
 	{
 		x = page_FIFO.front();
-		x1 = (int *)(page_table[x]);
-		b1 = *x1;
-		b2 = b1 % 2;
-		if(b2 == 0)
+		
+		if((page_table[x] % 2) == 0)
 		{
 			vir = x;
-			*phy = b1/8;
+			*phy = page_table[x]/8;
 			page_FIFO.pop();
 			break;	
 		}
 		else
 		{
-			b1 -= 1;
-			*x1 = b1;
+			page_table[x] -= 1;
 			page_FIFO.push(page_FIFO.front());
 			page_FIFO.pop();
 		} 
@@ -172,14 +134,7 @@ int SecChance(int * phy)
 void ReadWrite(int num, char rw,int LINE_NUMBER,int algo)
 {
 	//page table entry is not valid(valid == 0)
-	int * aa;
-	int aaa;
-	aa = (int *)(page_table[num]);
-	aaa = *aa;
-	aaa %= 8;
-	
-	//if((page_table[num][3] | 0xFB) == 0xFB)
-	if(aaa < 4)
+	if((page_table[num] % 8) < 4)
 	{
 		PAGE_FAULTS ++;	
 		int phy = freeframe();
@@ -204,14 +159,7 @@ void ReadWrite(int num, char rw,int LINE_NUMBER,int algo)
 			EXEC_CYCLE += 3501;
 			cout << LINE_NUMBER << ": "<<"UNMAP " << vir  << " "<< phy << endl;
 
-			int * bb;
-			int bbb;
-			bb = (int *)(page_table[vir]);
-			bbb = *bb;
-			bbb %= 4;
-
-			//if((page_table[vir][3] | 0xFD) == 0xFF) cout << "OUT" << vir  << " "<< phy <<endl;
-			if(bbb >= 2)
+			if(page_table[vir] % 4 >= 2) //if dirty
 			{
 				cout << LINE_NUMBER << ": "<<"OUT " << vir  << " "<< phy <<endl;
 				EXEC_CYCLE += 3000;	
@@ -222,17 +170,9 @@ void ReadWrite(int num, char rw,int LINE_NUMBER,int algo)
 		}
 		
 		//////copy the physical frame number into pagetable
-		int *x;
-		int b,c;
-		x = (int *)(page_table[num]);
-		b = *x;
-		if((b % 8) < 4) c = 8 * phy + (b % 8) + 4;
-		else c = 8 * phy + (b % 8);
-		//cout << "c " << c << endl;
-		//frameLine[phy] = LINE_NUMBER;
+		if((page_table[num] % 8) < 4) page_table[num] = 8 * phy + (page_table[num] % 8) + 4;
+		else page_table[num] = 8 * phy + (page_table[num] % 8);
 		page_FIFO.push(num);
-		*x = c; 
-		///page_table[num][3] = page_table[num][3] | 0x04; //valid bit is set to 1
 
 	}
 	else
@@ -240,29 +180,15 @@ void ReadWrite(int num, char rw,int LINE_NUMBER,int algo)
 		EXEC_CYCLE ++;
 		cout <<LINE_NUMBER << ": "<< "access memory" << endl;
 	} 
-	
+	// dirty bit is set one
 	if(rw == '1')
-	{
-		int *x1;
-		int b1;
-		x1 = (int *)(page_table[num]);
-		b1 = *x1;
-		if((b1 % 4 ) < 2)b1 = b1 + 2; 
-		*x1 = b1;
-		//page_table[num][3] |= 0x02; // dirty bit is set one
-	}
+		if((page_table[num] % 4 ) < 2)page_table[num] = page_table[num] + 2; 
 	
-	int *x2;
-	int b2;
-	x2 = (int *)(page_table[num]);
-	b2 = *x2;
-	if((b2 % 2) < 1)b2 = b2 + 1; 
-	*x2 = b2;
+	// referenced bit is set to 1
+	if((page_table[num] % 2) < 1)page_table[num] = page_table[num] + 1; 
 
 	page_LRU.remove(num);
 	page_LRU.push_back(num);
-	
-	//page_table[num][3] |= 0x01; // referenced bit is set to 1
 	return;
 }
 
@@ -288,7 +214,6 @@ void parse_page()
 				a.rw = line[0];
 				a.line_no = LINE_NUMBER;
 				IR_TABLE.push_back(a);
-				//ReadWrite(page_number,line[0]);
 			}
 		}
 		myfile.close();
@@ -321,8 +246,7 @@ void Initialize()
 	//Initializing valid,dirty and reference bits to be 0
 	for(int i = 0 ; i < 64; i++)
 	{
-		x = (int *)page_table[i];
-		*x = 0;
+		page_table[i] = 0;
 	}
 }
 
